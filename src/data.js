227505339db0259566541ee8f959cf225cc38261
unknown <palenciaandres503gmail.com>
2022-12-10 23:50:44 -0500

    version 1

---
+++ src/data.js
@@ -0,0 +1,57 @@
+const menu = [
+    {
+      id: 1,
+      title: 'Mezcla',
+      category: 'breakfast',
+      price: 38.999,
+      img: './images/mezcla.jpg',
+      desc: `Calle 11a#43d60
+      Restaurante Bar, donde se mezclan variedad de sabores artesanales, frescos y culturales. Un lugar para compartir momentos inolvidables`,
+    },
+    {
+      id: 2,
+      title: 'Tika Dogs Gourmet',
+      category: 'lunch',
+      price: 43.999,
+      img: './images/tika.jpg',
+      desc: `Carrera 34 # 7-29
+      Tika Dogs Gourmet, perros calientes diferentes y exquisitos realizados con los mejores ingredientes `,
+    },
+    {
+      id: 3,
+      title: 'Ammazza Gin Garden',
+      category: 'shakes',
+      price: 33.999,
+      img: './images/garden.jpg',
+      desc: `Carrera 34, 7-40
+      Queremos ser para ustedes una experiencia magnifica desde lo exquisito de nuestros platos al paladar, para así pasar por cada uno de los otros sentidos…La visión con el ambiente que les ofrecemos.`,
+    },
+    {
+      id: 4,
+      title: 'Malanga del Trópico',
+      category: 'breakfast',
+      price: 54.998,
+      img: './images/malanga.jpg',
+      desc: `Calle 38 79 46
+      Somos quien convoca alrededor de un buen plato de comida y hacemos de cada momento un sabor especial. El arrebato de tener un lugar que saca a los comensales del asfalto de la ciudad `,
+    },
+    {
+      id: 5,
+      title: 'Tal Cual',
+      category: 'lunch',
+      price: 22.999,
+      img: './images/talcual.jpg',
+      desc: `Calle 12, 43D-12
+      Un espacio para disfrutar de deliciosos platos que son presentados al comensal como deliciosas obras pictóricas además de ser una tienda de arte con espacio de galería para exposiciones `,
+    },
+    {
+      id: 6,
+      title: 'Mamasita Medallo',
+      category: 'shakes',
+      price: 32.997,
+      img: './images/mamasita.jpg',
+      desc: `Carrera 43d 10 77
+  Nuestra inspiración es la sabiduría criolla.Cócteles inspirados en el trópico y la mejor Comida Local,Hechos cuidadosamente a mano con los mejores ingredientes y recetas de Medallo.`,
+    }, 
+  ];
+  export default menu;
